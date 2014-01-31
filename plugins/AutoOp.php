b23923369169f8c1a173c3472d92770ba8b0c7b2
Jonathon <jonathon@fubra.com>
2014-01-31 18:25:05 +0000

    Added rough versions of AutoOp and ChannelLogger plugins.

---
+++ plugins/AutoOp.php
@@ -0,0 +1,38 @@
+<?php
+
+class AutoOp {
+
+	protected $_controller;
+	protected $_rules = array();
+
+	function __construct($config, $controller) {
+
+		$this->_controller = $controller;
+		
+	 // Do a little tidying on the configuration array if required...
+		foreach ($config AS $channel => $users) {
+			$this->_controller->add_bot_channel($channel);
+			$channel = $this->_controller->add_channel_hash($channel);
+			foreach ($users AS $user) {
+				$this->_rules[$channel][] = strtolower($user);
+			}
+		}
+
+	}
+
+	public function data_message($message) {
+
+		if (isset($message[1]) && ('JOIN' == $message[1])) {
+		
+			$channelName = trim(substr($message[2], 1));
+			list(, $nickname) = $this->_controller->parse_user_ident($message[0]);
+
+			if (isset($this->_rules[$channelName]) && in_array(strtolower($nickname), $this->_rules[$channelName])) {
+				$this->_controller->send_data('MODE '.$channelName.' +o '.$nickname);
+			}
+
+		}
+
+	}
+
+}
---
+++ plugins/ChannelLogger.php
@@ -0,0 +1,52 @@
+<?php
+
+class ChannelLogger {
+
+	protected $_controller;
+
+	function __construct($config, $controller) {
+	
+		$this->_controller = $controller;
+		$this->_controller->add_bot_channel($config);
+
+	}
+
+	public function channel_message(array $fromDetails, $channelName, $message) {
+	
+		if (substr($message, 1, 6) == 'ACTION') {
+			$this->_write_log($channelName, '* '.$fromDetails[1].' '.trim(substr($message, 8)));
+		} else {
+			$this->_write_log($channelName, $fromDetails[1].': '.trim($message));
+		}
+
+	}
+
+	public function data_message($message) {
+
+		if (isset($message[1])) {
+			if (':#' == substr($message[2], 0, 2)) {
+				$channelName = trim(substr($message[2], 1));
+			} else {
+				$channelName = trim($message[2]);
+			}
+			$fromDetails = $this->_controller->parse_user_ident($message[0]);
+
+			switch ($message[1]) {
+			  case 'JOIN':
+					$this->_write_log($channelName, '['.$fromDetails[1].' joined chat]');
+				break;
+				case 'PART':
+					$this->_write_log($channelName, '['.$fromDetails[1].' left chat]');
+				break;
+			}
+		}
+
+	}
+	
+	protected function _write_log($channel, $message) {
+	
+		file_put_contents('logs/'.$channel.'.log', '['.date('H:i:s').'] '.$message.PHP_EOL, FILE_APPEND);
+	
+	}
+
+}
