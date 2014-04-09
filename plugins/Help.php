ed358a33be7e782f12645b3d256e917718ea381d
ConfuzzledDuck <jonathon@fubra.com>
2014-04-09 17:24:45 +0000

    Expanded help functionality to work with plugin source data.

--- plugins/Help.php
+++ plugins/Help.php
@@ -14,6 +14,7 @@ class Help extends PluginAbstract {
 
 		$this->_controller->add_hooked_command('!help', 'Returns this help information.', 'help');
 		$this->_controller->add_hooked_command('!help [pluginname]', 'Returns help information for a currently loaded plugin.', 'help');
+		$this->_controller->add_hooked_command('!help general', 'Returns help information relating to general commands', 'help');
 
 	}
 
@@ -24,6 +25,7 @@ class Help extends PluginAbstract {
 
 		$this->_controller->remove_hooked_command('!help');
 		$this->_controller->remove_hooked_command('!help [pluginname]');
+		$this->_controller->remove_hooked_command('!help general');
 
 	}
 
@@ -80,11 +82,23 @@ class Help extends PluginAbstract {
 			}
 		} else {
 			if (count($commands) > 0) {
-				$response[] = 'The following commands can be used with '.$this->_controller->get_nickname().':';
-				foreach ($commands AS $source => $sourceCommands) {
-					foreach ($sourceCommands AS $command => $description) {
-						$response[] = '  '.$command.' - '.$description;
+				$helpParts = explode(' ', $message);
+				if (count($helpParts) == 2) {
+					if (array_key_exists($helpParts[1], $commands)) {
+						$response[] = 'The following commands can be used with the "'.$helpParts[1].'" plugin of '.$this->_controller->get_nickname().':';
+						foreach ($commands[$helpParts[1]] AS $command => $description) {
+							$response[] = '  '.$command.' - '.$description;
+						}
+					} else if ('general' == $helpParts[1]) {
+						$response[] = 'The following commands can be used with '.$this->_controller->get_nickname().':';
+						foreach ($commands['zzz'] AS $command => $description) {
+							$response[] = '  '.$command.' - '.$description;
+						}
+					} else {
+						$response[] = 'The plugin "'.$helpParts[1].'" is not currently running on '.$this->_controller->get_nickname().':';
 					}
+				} else {
+					$response[] = 'Invalid command.'
 				}
 			} else {
 				$response[] = $this->_controller->get_nickname().' does not respond to any commands.';
